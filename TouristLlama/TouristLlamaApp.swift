2967451d0788176f8531b5afe7ccc15e8cb8048d
Evgeny Mitko <emitko@data.ai>
2023-08-07 10:53:31 +0200

    Initial Commit

---
+++ TouristLlama/TouristLlamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouristLlamaApp.swift
+//  TouristLlama
+//
+//  Created by Evgeny Mitko on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouristLlamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
